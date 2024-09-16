1f4c83b2db0a6145fe8687e5a5d5028a0d1ad646
Gayathiri-Jeyananthan <gayathirijeyananthan@gmail.com>
2024-09-16 06:11:01 +0530

    Data types

---
+++ firstproject.py
@@ -0,0 +1,21 @@
+#First code & this is a comment
+
+print("Hello,World!")
+print("Hello Gayu")
+
+'''
+this is a multi line
+comment
+'''
+
+#Python data types
+x=1 #Integer -- int
+y=1.5 #Float -- float
+z=1j #Complex -- complex
+
+#To check the types of variables
+print(type(x))
+print(type(y))
+print(type(z))
+
+#Convert a number from integer to float
